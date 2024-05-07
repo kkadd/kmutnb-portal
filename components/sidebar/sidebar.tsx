2a93b3dfdf8fe9f84c8f47a77c7d376acbe5e29e
Siriwant <kadsiriwan@gmail.com>
2024-05-08 00:53:27 +0700

    add: sidebar details

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { usePathname } from "next/navigation";
-import { useSidebarContext } from "../layout/layoutContext";
+import { SidebarContext, useSidebarContext } from "../layout/layoutContext";
 import { Sidebar } from "./sidebar.styles";
+import { SidebarItem } from "./sidebarItem";
+import { LogoutIcon, ServiceIcon, StaffIcon } from "../icons";
+import { SidebarMenu } from "./sidebarMenu";
+import { Avatar } from "@nextui-org/react";
 
 export const SidebarWrapper = () => {
   const pathname = usePathname();
@@ -17,10 +21,48 @@ export const SidebarWrapper = () => {
           collapsed: collapsed,
         })}
       >
-        <div className={Sidebar.Header()}>Logo</div>
+        <div className={Sidebar.Header()}>
+          <h1 className="text-black">Logo</h1>
+        </div>
         <div className="flex flex-col justify-between h-full">
-          <div className={Sidebar.Body()}>body test</div>
-          <div className={Sidebar.Footer()}>footer test</div>
+          <div className={Sidebar.Body()}>
+            <div className="flex gap-4 justify-center">
+              <Avatar
+                classNames={{
+                  base: "bg-gradient-to-br from-[#EFBBB0] to-[#EFBBB0]",
+                }}
+                name="Siriwan"
+                src="https://lh3.googleusercontent.com/proxy/NhIBBFCYzsbouMb_7IwQt1v5qAQvjJ2DB1j8bVPt7NdciRpqObog2TY-nUdNDcDof4Ls00DNvCypnL4mE5GT9mfx8Lr7TK-8K2_iIqWvOGVAuuiW-Uy58hyMZ-MTf_-Y66sZMA6wVTyzVQOHx9Xx3XAb14QqdYy_cA"
+                size="md"
+                radius="lg"
+              />
+              <div>
+                <p className="text-base font-medium text-[#E16449]">
+                  Siriwan Tuha
+                </p>
+                <p className="text-[10px] text-[#afafaf]">
+                  s6303051623179@kmutnb.ac.th
+                </p>
+              </div>
+            </div>
+            <SidebarMenu title="Menu">
+              <SidebarItem
+                isActive={pathname === "/service-management"}
+                href="/service-management"
+                title="Service Management"
+                icon={<ServiceIcon />}
+              />
+              <SidebarItem
+                isActive={pathname === "/staff-management"}
+                href="/staff-management"
+                title="Staff Management"
+                icon={<StaffIcon />}
+              />
+            </SidebarMenu>
+          </div>
+          <div className={Sidebar.Footer()}>
+            <SidebarItem title="Log out" icon={<LogoutIcon />} />
+          </div>
         </div>
       </div>
     </aside>
--- components/sidebar/sidebarItem.tsx
+++ components/sidebar/sidebarItem.tsx
@@ -1,1 +1,46 @@
+import React from "react";
+import NextLink from "next/link";
+import { useSidebarContext } from "../layout/layoutContext";
+import clsx from "clsx";
 
+interface Props {
+  title: string;
+  icon: React.ReactNode;
+  isActive?: boolean;
+  href?: string;
+}
+
+export const SidebarItem = ({ icon, title, isActive, href = "" }: Props) => {
+  const { collapsed, setCollapsed } = useSidebarContext();
+
+  const handleClick = () => {
+    if (window.innerWidth < 768) {
+      setCollapsed();
+    }
+  };
+  return (
+    <NextLink
+      href={href}
+      className="text-default-900 active:bg-none max-w-full"
+    >
+      <div
+        className={clsx(
+          isActive
+            ? "bg-[#FF644B] [&_svg_path]:fill-white"
+            : "hover:bg-default-100",
+          "flex gap-2 w-full min-h-[44px] h-full items-center px-3.5 rounded-xl cursor-pointer transition-all duration-150 active:scale-[0.98]"
+        )}
+        onClick={handleClick}
+      >
+        {icon}
+        <span
+          className={clsx(
+            isActive ? "text-white font-medium" : "text-default-600 font-medium"
+          )}
+        >
+          {title}
+        </span>
+      </div>
+    </NextLink>
+  );
+};
