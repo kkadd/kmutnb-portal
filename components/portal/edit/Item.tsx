d32ca1b485d7e096cb2c498fe084ff80b1011b97
Siriwant <kadsiriwan@gmail.com>
2024-06-01 12:43:37 +0700

    add: shorttable dnd ui

--- components/portal/edit/Item.tsx
+++ components/portal/edit/Item.tsx
@@ -1,29 +1,57 @@
-import React, { forwardRef, HTMLAttributes, CSSProperties } from 'react';
+import React, { forwardRef, HTMLAttributes, CSSProperties } from "react";
+import { TItem } from "./editPortalPage";
+import { Card, Image } from "@nextui-org/react";
 
-export type ItemProps = HTMLAttributes<HTMLDivElement> & {
-    id: string;
-    withOpacity?: boolean;
-    isDragging?: boolean;
+type ItemProps = HTMLAttributes<HTMLDivElement> & {
+  item: TItem;
+  withOpacity?: boolean;
+  isDragging?: boolean;
 };
 
-const Item = forwardRef<HTMLDivElement, ItemProps>(({ id, withOpacity, isDragging, style, ...props }, ref) => {
+const Item = forwardRef<HTMLDivElement, ItemProps>(
+  ({ item, withOpacity, isDragging, style, ...props }, ref) => {
     const inlineStyles: CSSProperties = {
-        opacity: withOpacity ? '0.5' : '1',
-        transformOrigin: '50% 50%',
-        height: '140px',
-        width: '140px',
-        borderRadius: '10px',
-        cursor: isDragging ? 'grabbing' : 'grab',
-        backgroundColor: '#ffffff',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        boxShadow: isDragging ? 'rgb(63 63 68 / 5%) 0px 2px 0px 2px, rgb(34 33 81 / 15%) 0px 2px 3px 2px' : 'rgb(63 63 68 / 5%) 0px 0px 0px 1px, rgb(34 33 81 / 15%) 0px 1px 3px 0px',
-        transform: isDragging ? 'scale(1.05)' : 'scale(1)',
-        ...style,
+      opacity: withOpacity ? "0.5" : "1",
+      transformOrigin: "50% 50%",
+      height: "140px",
+      width: "140px",
+      borderRadius: "10px",
+      cursor: isDragging ? "grabbing" : "grab",
+      backgroundColor: "#ffffff",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      boxShadow: isDragging
+        ? "rgb(63 63 68 / 5%) 0px 2px 0px 2px, rgb(34 33 81 / 15%) 0px 2px 3px 2px"
+        : "rgb(63 63 68 / 5%) 0px 0px 0px 1px, rgb(34 33 81 / 15%) 0px 1px 3px 0px",
+      transform: isDragging ? "scale(1.05)" : "scale(1)",
+      ...style,
     };
 
-    return <div ref={ref} style={inlineStyles} {...props}>{id}</div>;
-});
-Item.displayName = 'Item';
+    return (
+      <div ref={ref} style={inlineStyles} {...props}>
+        <div className="grid justify-center items-center gap-2 h-[132px]">
+          <Card
+            className="justify-center items-center bg-white p-2 h-[100px] w-[100px]"
+            key={item.id}
+          >
+            <Image
+              src={item.imageUrl}
+              alt={`${item.id}`}
+              width={90}
+              height={90}
+            />
+          </Card>
+
+          <div className="grid justify-center items-center text-default-700 font-sansThai">
+            {item.name.substring(0, 12)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+Item.displayName = "Item";
+
 export default Item;
--- components/portal/edit/editPortalPage.tsx
+++ components/portal/edit/editPortalPage.tsx
@@ -1,67 +1,138 @@
 "use client";
-import React, { FC, useState, useCallback } from 'react';
+
+import React, { FC, useState, useCallback } from "react";
 import {
-    DndContext,
-    closestCenter,
-    MouseSensor,
-    TouchSensor,
-    DragOverlay,
-    useSensor,
-    useSensors,
-    DragStartEvent,
-    DragEndEvent,
-} from '@dnd-kit/core';
-import { arrayMove, SortableContext, rectSortingStrategy } from '@dnd-kit/sortable';
-import Grid from './Grid';
-import SortableItem from './SortableItem';
-import Item from './Item';
+  DndContext,
+  closestCenter,
+  MouseSensor,
+  TouchSensor,
+  DragOverlay,
+  useSensor,
+  useSensors,
+  DragStartEvent,
+  DragEndEvent,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  rectSortingStrategy,
+} from "@dnd-kit/sortable";
+import Grid from "./Grid";
+import SortableItem from "./SortableItem";
+import Item from "./Item";
 
-export const EditPortalPage: FC = () => {
-    const [items, setItems] = useState(Array.from({ length: 14 }, (_, i) => (i + 1).toString()));
-    const [activeId, setActiveId] = useState<string | null>(null);
-    const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
-
-    const handleDragStart = useCallback((event: DragStartEvent) => {
-        setActiveId(event.active.id as any);
-    }, []);
-    const handleDragEnd = useCallback((event: DragEndEvent) => {
-        const { active, over } = event;
-
-        if (active.id !== over?.id) {
-            setItems((items) => {
-                const oldIndex = items.indexOf(active.id as any);
-                const newIndex = items.indexOf(over!.id as any);
-
-                return arrayMove(items, oldIndex, newIndex);
-            });
-        }
-
-        setActiveId(null);
-    }, []);
-    const handleDragCancel = useCallback(() => {
-        setActiveId(null);
-    }, []);
-
-    return (
-        <DndContext
-            sensors={sensors}
-            collisionDetection={closestCenter}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-            onDragCancel={handleDragCancel}
-        >
-            <SortableContext items={items} strategy={rectSortingStrategy}>
-                <Grid columns={7}>
-                    {items.map((id) => (
-                        <SortableItem key={id} id={id} />
-                    ))}
-                </Grid>
-            </SortableContext>
-            <DragOverlay adjustScale style={{ transformOrigin: '0 0 ' }}>
-                {activeId ? <Item id={activeId} isDragging /> : null}
-            </DragOverlay>
-        </DndContext>
-    );
+export type TItem = {
+  id: number;
+  name: string;
+  imageUrl: string;
 };
 
+const testServiceDnd = [
+  {
+    id: 1,
+    name: "ระบบสารสนเทศเพื่องานทะเบียนนักศึกษา",
+    imageUrl: "https://reg.kmutnb.ac.th/registrar/assets/images/logo/logo.png",
+  },
+  {
+    id: 2,
+    name: "ICIT Account",
+    imageUrl: "https://account.kmutnb.ac.th/web/images/icit_account_logo.png",
+  },
+  {
+    id: 3,
+    name: "บริการเครือข่ายโรมมิ่งเพื่อการศึกษาและการวิจัย(eduroam)",
+    imageUrl: "http://authen.eduroam.kmutnb.ac.th/images/logo.jpg",
+  },
+  {
+    id: 4,
+    name: "บริการซอฟต์แวร์ลิขสิทธ์",
+    imageUrl:
+      "https://acdserv.kmutnb.ac.th/wp-content/themes/acdserv/images/kmutnb-logo.png",
+  },
+  {
+    id: 5,
+    name: "ระบบสารสนเทศเพื่องานทะเบียนนักศึกษา",
+    imageUrl: "https://reg.kmutnb.ac.th/registrar/assets/images/logo/logo.png",
+  },
+  {
+    id: 6,
+    name: "ICIT Account",
+    imageUrl: "https://account.kmutnb.ac.th/web/images/icit_account_logo.png",
+  },
+  {
+    id: 7,
+    name: "บริการเครือข่ายโรมมิ่งเพื่อการศึกษาและการวิจัย(eduroam)",
+    imageUrl: "http://authen.eduroam.kmutnb.ac.th/images/logo.jpg",
+  },
+  {
+    id: 8,
+    name: "บริการซอฟต์แวร์ลิขสิทธ์",
+    imageUrl:
+      "https://acdserv.kmutnb.ac.th/wp-content/themes/acdserv/images/kmutnb-logo.png",
+  },
+];
 
+export const EditPortalPage: FC = () => {
+  const [items, setItems] = useState<TItem[]>(testServiceDnd);
+  const [activeItem, setActiveItem] = useState<TItem>();
+
+  const sensors = useSensors(useSensor(MouseSensor), useSensor(TouchSensor));
+  const columns = Math.min(items.length, 7);
+
+  const handleDragStart = (event: DragStartEvent) => {
+    const { active } = event;
+    setActiveItem(items.find((item) => item.id === active.id));
+  };
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const activeItem = items.find((item) => item.id === active.id);
+    const overItem = items.find((item) => item.id === over.id);
+
+    if (!activeItem || !overItem) {
+      return;
+    }
+
+    const activeIndex = items.findIndex((item) => item.id === active.id);
+    const overIndex = items.findIndex((item) => item.id === over.id);
+
+    if (activeIndex !== overIndex) {
+      setItems((prev) => arrayMove<TItem>(prev, activeIndex, overIndex));
+    }
+    setActiveItem(undefined);
+  };
+
+  const handleDragCancel = () => {
+    setActiveItem(undefined);
+  };
+
+  // Button save
+  // const handleButtonClick = () => {
+  //   const itemIds = items.map((item) => item.id)
+  //   alert(itemIds)
+  // }
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+    >
+      <SortableContext items={items} strategy={rectSortingStrategy}>
+        <Grid columns={columns}>
+          {items.map((item) => (
+            <SortableItem key={item.id} item={item} />
+          ))}
+        </Grid>
+      </SortableContext>
+      <DragOverlay adjustScale style={{ transformOrigin: "0 0 " }}>
+        {activeItem ? <Item item={activeItem} isDragging /> : null}
+      </DragOverlay>
+    </DndContext>
+  );
+};
