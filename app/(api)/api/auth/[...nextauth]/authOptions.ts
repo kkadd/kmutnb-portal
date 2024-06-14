6f4ec833ea2c558c787cc6d6e0a16e8e1aeb948c
oak072544 <oak072544@gmail.com>
2024-06-14 18:24:26 +0700

    refactor: Update authOptions to handle session-based user information
    The authOptions.ts file has been updated to handle session-based user information. This change ensures that the user's username, email, account type, management role, and display name are properly assigned to the token and session objects. It also includes a fix for parsing the user's userInfo object when it is stored as a string. This improves the accuracy and consistency of user authentication and session management.
    
    Note: This commit message follows the established conventions in the repository.

--- app/(api)/api/auth/[...nextauth]/authOptions.ts
+++ app/(api)/api/auth/[...nextauth]/authOptions.ts
@@ -70,31 +70,29 @@ export const authOptions: NextAuthOptions = {
     async jwt({ token, user, session }) {
       console.log("jwt callback", { token, user, session });
       if (user && "userInfo" in user) {
-        return {
-          ...token,
-          name: user.userInfo.username,
-          email: user.userInfo.email,
-          account_type:
-            user.userInfo.account_type == "students"
-              ? "student"
-              : user.userInfo.account_type,
-          management_role: user.management_role,
-          displayname: user.userInfo.displayname,
-        };
+        const userInfo =
+          typeof user.userInfo === "string"
+            ? JSON.parse(user.userInfo)
+            : user.userInfo;
+        token.name = userInfo.username;
+        token.email = userInfo.email;
+        token.account_type =
+          userInfo.account_type == "students"
+            ? "student"
+            : userInfo.account_type;
+        token.management_role = user.management_role;
+        token.displayname = userInfo.displayname;
+        return token;
       }
       return token;
     },
     async session({ session, token, user }) {
       console.log("session callback", { session, token, user });
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          account_type: token?.account_type,
-          management_role: token?.management_role,
-          displayname: token?.displayname,
-        },
-      };
+      session.user.account_type = token?.account_type;
+      session.user.management_role = token?.management_role;
+      session.user.displayname = token?.displayname;
+
+      return session;
     },
   },
 };
