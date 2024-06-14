418fced6dccb677b17f51c17c32e5d66b772c5b3
oak072544 <oak072544@gmail.com>
2024-06-14 16:55:53 +0700

    refactor: Add Access Denied page
    A new file `page.tsx` has been added under the `access-denied` directory in the `kmutnb-portal` feature. This file contains the implementation of the `AccessDeniedPage` component, which displays a message indicating that the user does not have permission to access the page. This change ensures proper handling of access denied scenarios.
    
    Note: This commit message follows the established conventions in the repository.

---
+++ app/(features)/kmutnb-portal/access-denied/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AccessDeniedPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Access Denied</h1>
+      <p>Sorry, you do not have permission to access this page.</p>
+    </div>
+  );
+};
+
+export default AccessDeniedPage;
