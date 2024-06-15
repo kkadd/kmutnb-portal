e90f3032c760490cc712fe605a05061b31873391
siriwant <kadsiriwan@gmail.com>
2024-06-15 23:21:37 +0700

    add: ui access denine page

--- app/(features)/kmutnb-portal/access-denied/page.tsx
+++ app/(features)/kmutnb-portal/access-denied/page.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Image } from "@nextui-org/react";
 
 const AccessDeniedPage: React.FC = () => {
   return (
-    <div>
-      <h1>Access Denied</h1>
-      <p>Sorry, you do not have permission to access this page.</p>
+    <div className="grid justify-center items-center gap-4 w-full">
+      <Image
+        src="/cybersecurity.svg"
+        alt="empty image"
+        height={300}
+        width={500}
+      />
+      <div className="grid justify-center items-center text-2xl font-sansThai">
+        Access Denied
+      </div>
+      <div className="flex justify-center items-center text-lg font-sansThai">
+        <div>Sorry, you do not have permission to access this page.</div>
+      </div>
     </div>
   );
 };
