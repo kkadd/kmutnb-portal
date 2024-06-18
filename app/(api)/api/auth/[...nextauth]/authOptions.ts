8a4a080b928e68f720c30a7a55eac781ea5eff1e
oak072544 <oak072544@gmail.com>
2024-06-18 15:04:28 +0700

    refactor: Update authorization token variable name in authOptions.ts and route.ts
    The authorization token variable name has been updated from `process.env.ACCESS_TOKEN` to `process.env.NEXT_PUBLIC_ACCESS_TOKEN` in the `authOptions.ts` and `route.ts` files. This change ensures consistency and improves security by using the correct environment variable.
    
    Note: This commit message follows the established conventions in the repository.

--- app/(api)/api/auth/[...nextauth]/authOptions.ts
+++ app/(api)/api/auth/[...nextauth]/authOptions.ts
@@ -31,7 +31,7 @@ export const authOptions: NextAuthOptions = {
               method: "POST",
               headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
               },
               body: JSON.stringify({
                 username: credentials?.username,
--- app/(api)/api/management/userInfo/route.ts
+++ app/(api)/api/management/userInfo/route.ts
@@ -1,24 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: Request, res: NextResponse) {
-    try {
-        let { username } = await req.json();
-        const info = await fetch("https://api.account.kmutnb.ac.th/api/account-api/user-info", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${process.env.ACCESS_TOKEN}`
-            },
-            body: JSON.stringify({
-                username: username
-            }), // Assign the agent to the fetch configuration
-        });
-        const response = await info.json();
-        return NextResponse.json(response, { status: 200 });
-    }
-    catch (e: any) {
-        return NextResponse.json(e, { status: e.status });
-    }
+  try {
+    let { username } = await req.json();
+    const info = await fetch(
+      "https://api.account.kmutnb.ac.th/api/account-api/user-info",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
+        },
+        body: JSON.stringify({
+          username: username,
+        }), // Assign the agent to the fetch configuration
+      }
+    );
+    const response = await info.json();
+    return NextResponse.json(response, { status: 200 });
+  } catch (e: any) {
+    return NextResponse.json(e, { status: e.status });
+  }
 }
 
 /**
