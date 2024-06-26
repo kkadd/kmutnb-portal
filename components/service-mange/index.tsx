0316ce10091462460de16aed3098bf41e74f83c2
oak072544 <oak072544@gmail.com>
2024-06-26 09:38:49 +0700

    refactor: Improve filtering and sorting logic in ServiceManage component
    The code changes in the ServiceManage component enhance the filtering and sorting logic. The filteredServices useMemo hook now includes an additional condition to filter services based on the username and managementRole. The fetch request in the useEffect hook is now wrapped in a try-catch block to handle any errors. This improves the overall functionality and user experience of the ServiceManage component.
    
    Note: This commit message follows the established conventions in the repository.

--- components/service-mange/index.tsx
+++ components/service-mange/index.tsx
@@ -71,10 +71,13 @@ export const ServiceManage = () => {
   }, []);
 
   const filteredServices = useMemo(() => {
-    return service.filter((service) =>
+    const searchService = service.filter((service) =>
       service.serviceName.toLowerCase().includes(filterValue.toLowerCase())
     );
-  }, [filterValue, service]);
+    return searchService.filter(
+      (service) => service.username == username || managementRole == "admin"
+    );
+  }, [filterValue, managementRole, service, username]);
 
   const [isLoading, setLoading] = useState(true);
 
@@ -111,13 +114,18 @@ export const ServiceManage = () => {
     });
     setSortedServices(sorted);
     if (isLoading) {
-      fetch("/api/management/service/getServices")
-        .then((res) => res.json())
-        .then((data) => {
-          setService(data);
-          console.log(data);
-          setLoading(false);
-        });
+      try {
+        fetch("/api/management/service/getServices")
+          .then((res) => res.json())
+          .then((data) => {
+            setService(data);
+            console.log(data);
+            setLoading(false);
+          });
+      } catch (e) {
+        console.log(e);
+        setLoading(false);
+      }
     }
   }, [filteredServices, sortOrder, isLoading]);
 
@@ -172,62 +180,58 @@ export const ServiceManage = () => {
         <div className="grid grid-cols-3 grid-flow-row gap-4">
           {sortedServices
             .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
-            .map((service) =>
-              service.username == username || managementRole == "admin" ? (
-                <Card key={service._id} className="max-w-[400px]" shadow="sm">
-                  <CardHeader className="flex gap-3">
-                    <Image
-                      alt="service img"
-                      height={40}
-                      width={40}
-                      radius="sm"
-                      src={service.serviceImg}
-                    />
-                    <div className="flex flex-col">
-                      <span className="text-md font-sansThai">
-                        {service.serviceName.substring(0, 45)}
-                      </span>
-                      <Link
-                        isExternal
-                        className="text-sm text-default-500"
-                        href={
-                          service.serviceLink.startsWith("http://") ||
-                          service.serviceLink.startsWith("https://")
-                            ? service.serviceLink
-                            : `https://${service.serviceLink}`
-                        }
-                      >
-                        {service.serviceLink.substring(0, 45)}
-                      </Link>
-                    </div>
-                  </CardHeader>
-                  <CardBody>
-                    <RoleChips roles={service.role} />
-                  </CardBody>
-                  <Divider />
-
-                  <CardFooter className="flex justify-between items-center">
-                    <Button
-                      className="mx-auto bg-transparent text-[#afafaf] w-full"
-                      onClick={() =>
-                        router.push("/management/services/edit/" + service._id)
+            .map((service) => (
+              <Card key={service._id} className="max-w-[400px]" shadow="sm">
+                <CardHeader className="flex gap-3">
+                  <Image
+                    alt="service img"
+                    height={40}
+                    width={40}
+                    radius="sm"
+                    src={service.serviceImg}
+                  />
+                  <div className="flex flex-col">
+                    <span className="text-md font-sansThai">
+                      {service.serviceName.substring(0, 45)}
+                    </span>
+                    <Link
+                      isExternal
+                      className="text-sm text-default-500"
+                      href={
+                        service.serviceLink.startsWith("http://") ||
+                        service.serviceLink.startsWith("https://")
+                          ? service.serviceLink
+                          : `https://${service.serviceLink}`
                       }
                     >
-                      Edit
-                    </Button>
-                    <Divider orientation="vertical" />
-                    <Button
-                      className="mx-auto bg-transparent text-[#afafaf] w-full"
-                      onPress={(e) => delClick(service._id)}
-                    >
-                      Delete
-                    </Button>{" "}
-                  </CardFooter>
-                </Card>
-              ) : (
-                <></>
-              )
-            )}
+                      {service.serviceLink.substring(0, 45)}
+                    </Link>
+                  </div>
+                </CardHeader>
+                <CardBody>
+                  <RoleChips roles={service.role} />
+                </CardBody>
+                <Divider />
+
+                <CardFooter className="flex justify-between items-center">
+                  <Button
+                    className="mx-auto bg-transparent text-[#afafaf] w-full"
+                    onClick={() =>
+                      router.push("/management/services/edit/" + service._id)
+                    }
+                  >
+                    Edit
+                  </Button>
+                  <Divider orientation="vertical" />
+                  <Button
+                    className="mx-auto bg-transparent text-[#afafaf] w-full"
+                    onPress={(e) => delClick(service._id)}
+                  >
+                    Delete
+                  </Button>{" "}
+                </CardFooter>
+              </Card>
+            ))}
         </div>
         <div className="flex justify-center items-center mt-10 w-full">
           <Pagination
@@ -240,7 +244,7 @@ export const ServiceManage = () => {
             size="sm"
             variant="flat"
             showControls
-            total={Math.ceil(service.length / itemsPerPage)}
+            total={Math.ceil(filteredServices.length / itemsPerPage)}
             initialPage={currentPage}
             onChange={(page) => setCurrentPage(page)}
           />
